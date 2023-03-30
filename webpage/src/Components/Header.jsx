f50d4df20b94d8a50d5ff974f0a1ad75bdfbeee4
Jared Flores <90533617+JaredFG@users.noreply.github.com>
2023-03-30 13:02:31 -0600

    Update Header.jsx

--- webpage/src/Components/Header.jsx
+++ webpage/src/Components/Header.jsx
@@ -1,9 +1,3 @@
-/**
- * Header component
- *
- * Top navigation bar for your site. Set to remain visible as the
- * user scrolls so that they can constantly reach any part of your page.
- */
 import React from "react";
 
 const Header = () => {
@@ -13,6 +7,7 @@ const Header = () => {
         position: "fixed",
         display: "flex",
         justifyContent: "center",
+        flexWrap: "wrap", // agregamos flexWrap para envolver elementos
         gap: "2rem",
         background: "rgba(255,255,255,0.75)",
         backdropFilter: "blur(10px)",
